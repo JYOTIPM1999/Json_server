ace2738ce1d117b6cfc36d3a98166af282eb5230
JYOTIPM1999 <jyotipm1999@gmail.com>
2023-01-04 23:53:44 +0530

    Json

--- index.js
+++ index.js
@@ -23,21 +23,21 @@
 // server.listen(3000, () => {
 //   console.log("JSON Server is running");
 // });
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
 
-const PORT = 8000
+const PORT = 8000;
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
