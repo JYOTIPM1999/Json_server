e75697172c2565148ab68df27a4a67af20305da2
JYOTIPM1999 <jyotipm1999@gmail.com>
2023-01-04 23:25:46 +0530

    server

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-const command = `json-server --watch db.json --port ${port}`;
+// const command = `json-server --watch db.json --port ${port}`;
 
-exec(command, (err, stdout, stderr) => {
-  if (err) {
-    console.log("Error running exec", err);
-    return;
-  }
-  console.log("stdout:", stdout);
-  console.log("stderr:", stderr);
-});
+// exec(command, (err, stdout, stderr) => {
+//   if (err) {
+//     console.log("Error running exec", err);
+//     return;
+//   }
+//   console.log("stdout:", stdout);
+//   console.log("stderr:", stderr);
+// });
 
 // const jsonServer = require("json-server");
 // const server = jsonServer.create();
@@ -23,3 +23,21 @@ exec(command, (err, stdout, stderr) => {
 // server.listen(3000, () => {
 //   console.log("JSON Server is running");
 // });
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
