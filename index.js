9fc727906034a03f775e88c3a9500446f66637f3
Jyoti prakash Mohapatra <101392884+JYOTIPM1999@users.noreply.github.com>
2023-01-04 23:22:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-// const { exec } = require("child_process");
+const { exec } = require("child_process");
 
-// // Set port (default: 3000). For Heroku, we need to use
-// // the port set by the environment variable $PORT
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-// const command = `json-server --watch db.json --port ${port}`;
+const command = `json-server --watch db.json --port ${port}`;
 
-// exec(command, (err, stdout, stderr) => {
-//   if (err) {
-//     console.log("Error running exec", err);
-//     return;
-//   }
-//   console.log("stdout:", stdout);
-//   console.log("stderr:", stderr);
-// });
-
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log("Error running exec", err);
+    return;
+  }
+  console.log("stdout:", stdout);
+  console.log("stderr:", stderr);
 });
+
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+// server.listen(3000, () => {
+//   console.log("JSON Server is running");
+// });
